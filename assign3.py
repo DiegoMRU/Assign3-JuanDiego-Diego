52e4ce7e4bdcec5e2a311061805e92a087cb89ec
Diego Gonzalez <dgonz348@mtroyal.ca>
2022-10-24 15:41:52 -0600

    Initial Commit

---
+++ assign3.py
@@ -0,0 +1,33 @@
+USERNAME = "dgonz348"  # define your MRU username here
+
+
+def get_beverage_type(a_enabled: bool, b_enabled: bool) -> str:
+    """
+    Returns the beverage type defined by the switches.
+    """
+    if a_enabled == True:
+        
+        
+
+
+def get_temperature_desc(slider_value: int) -> str:
+    """
+    Returns the temperature description defined by the slider value.
+    Assume the value is always an integer between 0 and 100 (inclusive).
+    """
+
+
+def get_switch_value(switch_name: str) -> bool:
+    """
+    Prompts the user for the state of the specified switch.
+    Returns true if the specified switch is enabled and false otherwise.
+    """
+
+
+def main() -> None:
+    """
+    Prompts the user for the state of switches A and B and
+    the value of the numeric slider. Using the various provided
+    function headers, duplicate the functionality of the
+    abandoned replicator at https://mru-replicator.fly.dev.
+    """
