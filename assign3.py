44bc38b53907a0177f2505ac85f2c826ebf8cc41
Diego Gonzalez <dgonz348@mtroyal.ca>
2022-10-25 17:21:29 -0600

    program runs. it can be improved tho. Further testing needs to be done

--- assign3.py
+++ assign3.py
@@ -2,38 +2,27 @@ USERNAME = "dgonz348"  # define your MRU username here
 
 
 
- 
-
-
 def get_beverage_type(a_enabled: bool, b_enabled: bool) -> str:
     """
     Returns the beverage type defined by the switches.
     """
+    is_a_enabled = a_enabled # set the variable to the parameter's value
+    is_b_enabled = b_enabled # set the variable to the parameter's value
 
-    if a_enabled == 'y':
-        a_enabled = True
-    else: 
-        a_enabled = False
-    
-    if b_enabled == 'y':
-        b_enabled = True
-    else:
-        b_enabled = False
-
-    a_enabled = True
-    b_enabled = True
-
-
-
-    if a_enabled == True:
+    if (is_a_enabled == True) and (is_b_enabled == True):
         beverage = 'water'
-    elif b_enabled == True:
+    elif (is_a_enabled == True) and (is_b_enabled == False):
+        beverage = 'water'
+    elif (is_a_enabled == False) and (is_b_enabled == True):
         beverage = 'milk, 2%'
-    elif a_enabled  == False and b_enabled == False:
+    elif (is_a_enabled == False) and (is_b_enabled == False):
         beverage = 'coffee, americano'
-    # it compares the values for the switches A and B and assigns the values depending  what the users chooses
-    # this is basic code and needs to be improved
-    print(beverage)
+    # it compares the values of is_a_enabled and is_b_enabled and depending on 
+    # the values it sets the variable to a string from above
+    
+    message = beverage # it sets the message variable to what the beverage was set for
+    return message
+    
 
 
 
@@ -42,43 +31,37 @@ def get_temperature_desc(slider_value: int) -> str:
     Returns the temperature description defined by the slider value.
     Assume the value is always an integer between 0 and 100 (inclusive).
     """
-    slider_value = int(input('enter number'))
-    if slider_value == 0:
-        print('frozen')
-    elif slider_value <= 15:
-        print('cold')    
-    elif slider_value <=41:
-        print('warm') 
-    elif slider_value <= 99:
-        print('hot')
-    elif slider_value == 100:
-        print('boiling')
-    # It compares the values of what the user chooses and print a state of the beverage depending on what the user chose
-    # this is basic code and needs to be improved
+    is_slider_value = slider_value # sets the variable to the parameter's value
+
+    if is_slider_value == 0:
+        state = 'frozen'
+    elif is_slider_value <= 15:
+        state = 'cold'    
+    elif is_slider_value <=41:
+        state = 'warm' 
+    elif is_slider_value <= 99:
+        state = 'hot'
+    elif is_slider_value == 100:
+        state = 'boiling'
+    # It compares the value the user chose and then it assigns a string to state variable 
+    
+    message = state # assigns variable to state
+    return message
 
 def get_switch_value(switch_name: str) -> bool:
     """
     Prompts the user for the state of the specified switch.
     Returns true if the specified switch is enabled and false otherwise.
     """
-    switch_name = input("Which switch is enabled?(A or B): ").upper()
+    is_switch_name = switch_name # sets the variable to the parameter's value 
 
-    if switch_name != 'A' or switch_name != 'B':
-        while switch_name != 'A' and switch_name != 'B':
-            switch_name = input('Invalid entry, please choose between A and B: ')
+    if is_switch_name == 'y':
+        switch_name = True
+    elif is_switch_name == 'n':
+        switch_name = False
+    # It compares the value and depending on the string it sets switch_name to either True or False
 
-    switch_enabled = input(f"Is {switch_name} enabled?(y for yes and n for no): ").lower()
-
-    if switch_enabled != 'y' or switch_enabled != 'n':
-        while switch_enabled != 'y' and switch_enabled != 'n':
-            switch_enabled = input('Invalid entry, please use y or n: ')
-        # use to prompt the user to put a valid input
-        # reference for how to use while loops is from python crash course pg 118-119 Introducing while loops     
-    if switch_enabled == 'y':
-        switch_enabled = True
-    elif switch_enabled == 'n':
-        switch_enabled = False
-
+    return switch_name
 
 def main() -> None:
     """
@@ -87,10 +70,21 @@ def main() -> None:
     function headers, duplicate the functionality of the
     abandoned replicator at https://mru-replicator.fly.dev.
     """
-    switch_a = input("Is switch A enabled?(y/n): ")
-    switch_b = input("Is switch B enabled?(y/n): ")
-    slider_value = input("What's the value of the slider?(0-100): ")
     
-    get_beverage_type()
-    get_temperature_desc()
-    get_switch_value()
+    switch_a = get_switch_value(input("Is switch A enabled?(y/n): "))
+    # it sets the argument to what the user input and then calls in the function 
+    # to then set the variable to what the function returned
+    
+    switch_b = get_switch_value(input("Is switch B enabled?(y/n): "))
+    # it sets the argument to what the user input and then calls in the function 
+    # to then set the variable to what the function returned
+
+    slider_value = int(input("What's the value of the slider?(0-100): "))
+    #set the variable to what the user input, the value is a integer
+    
+
+    print(f'result: {get_beverage_type(switch_a, switch_b)}, {get_temperature_desc(slider_value)}')
+    # calls in the functions "get_beverage_type" and "get_temperature_desc" using the values from the previous variables
+    # as arguments and then it prints the result  
+
+main()
