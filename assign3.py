a6f3ec768c459fc3d38f59e86bda57bc080a17fb
Diego Gonzalez <dgonz348@mtroyal.ca>
2022-11-10 11:16:33 -0700

    testing the git

--- assign3.py
+++ assign3.py
@@ -9,12 +9,12 @@ def get_beverage_type(a_enabled: bool, b_enabled: bool) -> str:
     is_a_enabled = a_enabled # set the variable to the parameter's value
     is_b_enabled = b_enabled # set the variable to the parameter's value
 
-    if (is_a_enabled == True):
-        beverage = 'water, '
-    # Since water is obtained by the swicth A being enabled, either B is enabled or not, 
-    # there is no need to care for B being True or False
+    if is_a_enabled:
+        beverage = 'water '
+    # Since water is obtained by the switch A being enabled, whether B is enabled or not, 
+    # then there is no need to care for B being True or False
 
-    elif (is_a_enabled == False) and (is_b_enabled == True):
+    elif not is_a_enabled and is_b_enabled:
         beverage = 'milk, 2%'
     else:
         beverage = 'coffee, americano'
@@ -46,7 +46,7 @@ def get_temperature_desc(slider_value: int) -> str:
  / /           
 (_/  
                 """       
-    elif slider_value < 16:
+    elif slider_value < 16: # From 1 to 15 
         state = """cold
         
     .   *   ..  . *  * 
@@ -63,7 +63,7 @@ def get_temperature_desc(slider_value: int) -> str:
     |\_|__|__|_/|      
      \_________/  
                 """    
-    elif slider_value < 42:
+    elif slider_value < 42: # From 16 to 41
         state = """warm
      ____     
     |    |    
@@ -85,7 +85,7 @@ def get_temperature_desc(slider_value: int) -> str:
 |------------|
 |____________|
                 """ 
-    elif slider_value < 100:
+    elif slider_value < 100: # From 42 to 99
         state = """hot
     (  )  (   )  )      
      )(    ) (  (       
@@ -113,7 +113,7 @@ def get_temperature_desc(slider_value: int) -> str:
               |                   |
                `""----..____..---"" 
                 """
-    # It compares the value the user chose and then it assigns a string to state variable 
+    # Depending on the user's choice, it assigns a value to the variable state 
 
     return state
 
@@ -126,14 +126,10 @@ def get_switch_value(switch_name: str) -> bool:
     """
       
 
-    is_switch_name = input(f"Is switch {switch_name} enabled?(y/n): ").lower() == 'y'
+    is_user_input = input(f"Is switch {switch_name} enabled?(y/n): ").lower() == 'y'
     #it prompts the user for the state of the swicth and it  compare that input to make it  a boolean 
     # and it assigns that to the variable
-    
-    if is_switch_name == True:
-        return True
-    else:
-        return False
+    return is_user_input
     # It compares the value and depending on the string it returns either True or False
 
 
@@ -145,19 +141,22 @@ def main() -> None:
     function headers, duplicate the functionality of the
     abandoned replicator at https://mru-replicator.fly.dev.
     """
-    switch_a = get_switch_value("A")
+    is_switch_a = get_switch_value("A")
     # Calls in "get_switch_value" for the switch A and then it assigns what the function returned to switch_a
     
-    switch_b = get_switch_value("B")
+    is_switch_b = get_switch_value("B")
     # Calls in "get_switch_value" for the switch B and then it assigns what the function returned to switch_b
     
     slider_value = int(input("What's the value of the slider?(0-100): "))
     # set the variable to the user's input, the input should be an integer
     
 
-    print(f'Result: {get_beverage_type(switch_a, switch_b)}, {get_temperature_desc(slider_value)}')
+    print(f'Result: {get_beverage_type(is_switch_a, is_switch_b)}, {get_temperature_desc(slider_value)}')
     # calls in the functions "get_beverage_type" and "get_temperature_desc" using 
     # the values from the previous variables as arguments and then it prints the result  
+    while slider_value <= 100:
+        slider_value += 1
+        main()
 
 main() # calls in main to execute program
 
